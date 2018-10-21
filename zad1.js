41382bad3abc737b8b1fbc1aac3e804554fbc62c
KrzysKolo <krzysztofa.kolodziejczak@gmail.com>
2018-10-21 22:33:19 +0200

    node.js_1

--- zad1.js
+++ zad1.js
@@ -1,12 +1,17 @@
 process.stdin.setEncoding('utf8');
-process.stdin.on('readable', function(){
-var input = process.stdin.read();
-if(input !== null){
-    var instruction = input.toString().trim();
-    if(instruction === 'exit') {
-       process.abort(); 
-    } else {
-    console.log('Wpisałeś tekst:  '+input.toString());
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch(instruction) {
+            case 'exit':
+                process.exit();
+            case 'info':
+                console.log(process.platform);
+                console.log(process.version);
+                break;
+            default:
+                console.log('Wpisałeś tekst:  '+input.toString());
+        }
     }
-}
-})
+});
